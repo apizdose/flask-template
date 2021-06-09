79e55bfd96ce2907e920b4955a6e207f8164c212
duffer <you@example.com>
2021-06-09 16:01:20 +0300

    Adding env variables

--- flask/app.py
+++ flask/app.py
@@ -4,17 +4,17 @@ from flask import Flask
 import os
 import socket
 
-hostname = socket.gethostname()
+HOSTNAME = socket.gethostname()
 
 try:
-    ENVIR=os.environ["ENVIR"]
-except: ENVIR='<br><br><center><h1> Need to define env var in .env file!'    
+    CONTENT=os.environ["CONTENT"]
+except: CONTENT='<br><br><center><h1> Need to define env var in .env file!'    
 
 app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return f'{ENVIR} {hostname}'
+    return f'{CONTENT} {HOSTNAME}'
         
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
